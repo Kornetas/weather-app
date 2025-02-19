7a98c0432360b5f70c40baec5012affedb1fdc35
Arkadiusz Rak <arek93r@gmail.com>
2025-02-19 10:39:00 +0100

    wip

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("searchBtn").addEventListener("click", function () {
+  let city = document.getElementById("cityInput").value;
+  console.log("Wyszukuję pogodę dla:", city);
+});
